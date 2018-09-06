879849dddde12b48ed6b2a86b5ef6b99c76fa78e
jedusaad <jedusaad@gmail.com>
2018-09-06 08:22:12 -0300

    Reseting repository

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# simpler-npm-mysql
+# Simpler-Mysql
+
+A package made to make your life easyer when dealing with MYSQL!
+
+### How to work with:
+
+```sh
+$ var sMysql = require('simpler-mysql');
+
+$ sMysql.config(host, user, password, database));
+```
+
+Whit this done you have two options:
+The first is to work with single-queries connection, the library will connect to the database, do the transation and close the connection. 
+```sh
+$ sMysql.execute('SQL_STATEMENT');
+// OR 
+$ sMysql.execute( 'SQL_WITH_PREPARE_?_STATEMENT_?' , [ arg1 , arg2 ] );
+```
+The second way is to connect, execute any amount of queries and then close the connection.
+```sh
+$ sMysql.connect();
+// THEN  
+$ sMysql.execute( 'SQL_WITH_PREPARE_?_STATEMENT_?_1' , [ arg1 , arg2 ] );
+$ sMysql.execute( 'SQL_WITH_PREPARE_?_STATEMENT_?_2' , [ arg1 , arg2 ] );
+$ sMysql.execute( 'SQL_WITH_PREPARE_?_STATEMENT_?_3' , [ arg1 , arg2 ] );
+// THEN
+$ sMysql.close();
+```
+> Note: If you leave the connection open, it will overload your database with unecessary connections and maybe causing it to crash!
---
+++ index.js
@@ -0,0 +1,98 @@
+/**
+ * Database module
+ * @author João Eduardo Saad
+ * @since 21/12/2017
+ *
+ */
+
+var mysql = require('mysql');
+const chalk = require('chalk');
+
+    // DECLARING CONFIG PROPERTY OF MODULE
+    var configuration = false;
+    var connection = false;
+
+    //CONFIGURATION FUNCTION TO SET UP CONNECTION INFO
+    module.exports.config = async function (host, user, password, database) {
+        this.configuration = {
+            host: host,
+            user: user,
+            password: password,
+            database: database
+        }
+    }
+
+    module.exports.query = async function (sql, args = null) {
+        // VAR THAT DEFINES IF CONNECTION WILL CLOSE AUTOMATICALLY :: DEFAULT :: TRUE
+        var close = true;
+        if (!this.connection) {
+            if (this.configuration) {
+                // CONNECT TO DB
+                this.connection = mysql.createConnection(this.configuration);
+            }else{
+                // SEND ERROR TELLING THAT CANNOT CONNECT IF THERE IS NO CONFIGURATION
+                console.log(new Date().toISOString() + ' : ' + (chalk.red("Cannot execute a query without a connection or configuration!")));
+                return null;
+            }
+        }else{
+            // IF THERE WAS A CONNECTION PRIOR TO THIS FUNCTION EXECUTION THE CONNECTION WILL NOT CLOSE AUTOMATICALLY
+            close = false;
+        }
+                
+        // AFTER CONFIG IS SET CALLS THE QUERY
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, (err, rows) => {
+                if (err)
+                    return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+
+
+    // CLOSING CONNECTION IF EXISTS    
+    module.exports.close = async function() {
+        if (this.connection) {
+            return new Promise((resolve, reject) => {
+                this.connection.end(err => {
+                    if (err)
+                        return reject(err);
+                    resolve();
+                });
+            });    
+        }else{
+            console.log(new Date().toISOString() + ' : ' + (chalk.yellow("Cannot close a connection that doesn't exists!")));
+            return null;
+        }    
+    }
+
+    // EXECUTE ANY QUERY WITH OR WIHOUT SQL INJECT PROTECTION 
+    module.exports.execute = async function (query) {
+        var result = await this.query(query.sql, query.args)
+            .then(rows => {
+                // console.log(rows);
+                return rows;
+            });
+        await this.close()
+            .then(() => {
+            });
+        return result;
+    }
+    
+    // PRINTS THE CONFIGURATION OF THE DATABASE
+    module.exports.teste = async function(){
+        console.log(this.configuration);
+        
+    }
+
+    // FUNCTION TO CONNECT TO THE DATABASE 
+    module.exports.connect = async function(){
+        if (this.configuration) {
+            this.connection = mysql.createConnection(this.configuration);
+        }else{
+            console.log(new Date().toISOString() + ' : ' + (chalk.red("Cannot connect to a database without a configuration! ")));
+            return null;
+        }
+    }
+
+
